d5d4cb011f3837a6063fa2530ac8f71e200051bc
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-03-10 17:40:34 +0530

    fixes

--- vite-project/src/hooks/useNowPlayingMovies.js
+++ vite-project/src/hooks/useNowPlayingMovies.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
- import { useDispatch ,useSelectory} from "react-redux";
+ import { useDispatch,useSelector } from "react-redux";
  import { API_OPTIONS } from "../utils/constants";
  import { addNowPlayingMovies } from "../utils/moviesSlice";
  
